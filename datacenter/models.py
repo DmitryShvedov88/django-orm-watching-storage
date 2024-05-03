22b2d6eb4e1ed61ec5408940eb68ab5d02ea67c5
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-05-03 19:43:10 +0300

    improvement 5 init func count_duration

--- datacenter/models.py
+++ datacenter/models.py
@@ -38,17 +38,21 @@ def format_duration(duration):
     return duration
 
 
-def get_duration(visit):
-    entered_at = visit.entered_at
-    entered_at = django.utils.timezone.localtime(entered_at)
-    leaved_at = visit.leaved_at
+def count_duration(leaved_at, entered_at):
     if leaved_at:
-        leaved_at = visit.leaved_at
         leaved_at = django.utils.timezone.localtime(leaved_at)
         duration = leaved_at-entered_at
     else:
         localtime = django.utils.timezone.localtime()
-        duration = localtime-entered_at  
+        duration = localtime-entered_at 
+    return duration
+
+
+def get_duration(visit):
+    entered_at = visit.entered_at
+    entered_at = django.utils.timezone.localtime(entered_at)
+    leaved_at = visit.leaved_at
+    duration = count_duration(leaved_at, entered_at)
     duration = format_duration(duration)
     return duration
 
@@ -57,12 +61,6 @@ def is_visit_long(visit, minutes=60):
     entered_at = visit.entered_at
     entered_at = django.utils.timezone.localtime(entered_at)
     leaved_at = visit.leaved_at
-    if leaved_at:
-        leaved_at = visit.leaved_at
-        leaved_at = django.utils.timezone.localtime(leaved_at)
-        duration = leaved_at-entered_at
-    else:
-        localtime = django.utils.timezone.localtime()
-        duration = localtime-entered_at
+    duration = count_duration(leaved_at,  entered_at)
     duration = int((duration.total_seconds())//minutes)
     return duration > 60
